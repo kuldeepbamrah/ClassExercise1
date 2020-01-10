5b1366db4763ba1597271f7ff84f0256a33e0b80
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-09 20:20:50 -0500

    initial commit

--- ClassExercise1/ViewController.swift
+++ ClassExercise1/ViewController.swift
@@ -13,8 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let viewToAnimate = UIView(frame: CGRect(x: 75, y: 75, width: 75, height: 75))
+        viewToAnimate.backgroundColor = UIColor.blue
+        view.addSubview(viewToAnimate)
+        
+        
+        let startPoint = CGPoint(x: 0, y: 0)
+        let endPoint = CGPoint(x: 0, y: 900)
+        let duration: Double = 6.0
+        
+        let positionAnimation = constructPositionAnimation(startingPoint: startPoint, endPoint: endPoint, animationDuration: duration)
+        
+        viewToAnimate.layer.add(positionAnimation, forKey: "position")
     }
 
-
+    private func constructPositionAnimation(startingPoint: CGPoint, endPoint: CGPoint, animationDuration: Double) -> CABasicAnimation {
+        let positionAnimation = CABasicAnimation(keyPath: "position")
+        positionAnimation.fromValue = NSValue(cgPoint: startingPoint)
+        positionAnimation.toValue = NSValue(cgPoint: endPoint)
+        positionAnimation.duration = animationDuration
+        positionAnimation.autoreverses = true
+        positionAnimation.repeatCount = Float.infinity
+        positionAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        return positionAnimation
+    }
 }
 
