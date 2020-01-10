f2cc1a4764faa0f647d5d34d753b0f134b79ba15
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-10 17:48:22 -0500

    done

--- ClassExercise1/ViewController.swift
+++ ClassExercise1/ViewController.swift
@@ -12,10 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var blockToAnimate: UILabel!
     
-    
+    var count = 0
+    var sRight = false
+    var sLeft = false
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        view.addGestureRecognizer(swipeLeft)
     }
     
     override func viewDidAppear(_ animated: Bool)
@@ -23,20 +33,46 @@ class ViewController: UIViewController {
         moveDown()
     }
 
+    //function to move down
    func moveDown()
    {
-        self.blockToAnimate.frame.origin.x = self.view.safeAreaInsets.left + 10
-        self.blockToAnimate.frame.origin.y = self.view.safeAreaInsets.top
+//        self.blockToAnimate.frame.origin.x = self.view.safeAreaInsets.left + 10
+//        self.blockToAnimate.frame.origin.y = self.view.safeAreaInsets.top
         UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear, animations:
             {
                 self.blockToAnimate.frame.origin.y =  self.view.frame.size.height -  self.view.safeAreaInsets.bottom
             },
             completion:
-            { finished in self.moveRight()
+           { finished in
+            if self.count == 1
+            {
+                self.moveLeft()
+            }
             })
     
     }
     
+    //function to move right
+    func moveRight()
+    {
+        UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear, animations:
+                    {
+                        let guide = self.view.safeAreaLayoutGuide
+                        let widht = guide.layoutFrame.size.width
+                        self.blockToAnimate.frame.origin.x = widht - 50
+    
+                    }, completion: { finished in
+                        if self.count == 0{
+                        self.moveUp()
+                        }
+                        else
+                        {
+                        self.moveDown()
+                        }
+                    })
+    }
+    
+    //function to move up
     func moveUp()
     {
         
@@ -44,10 +80,18 @@ class ViewController: UIViewController {
             {
                 self.blockToAnimate.frame.origin.y = self.view.safeAreaInsets.bottom
             },
-            completion: { finished in self.moveLeft()
+            completion: { finished in
+                if self.count == 0{
+                self.moveLeft()
+                }
+                else
+                {
+                self.moveRight()
+                }
             })
     }
     
+    //function to move left
     func moveLeft()
     {
         
@@ -59,28 +103,47 @@ class ViewController: UIViewController {
                         
         
             },
-            completion: { finished in self.moveDown()
+                       completion: { finished in
+                        if self.count == 0
+                       {
+                       self.moveDown()
+                       }
+                        else
+                        {
+                            if self.sRight == true
+                            {
+                            self.moveUp()
+                            }
+                        }
+                    
             })
         }
     
-    func moveRight()
-    {
-        UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear, animations:
-                    {
-                        let guide = self.view.safeAreaLayoutGuide
-                        let widht = guide.layoutFrame.size.width
-                        self.blockToAnimate.frame.origin.x = widht - 50
-                        print(self.view.bounds.maxX)
-                        print(widht)
-                        
+    @objc func swiped(gesture: UISwipeGestureRecognizer) {
         
-                }, completion: { finished in
-                    self.moveUp()
-                }
-           
-        )
-        
+        let swipeGesture = gesture as UISwipeGestureRecognizer
+        switch swipeGesture.direction {
+        case UISwipeGestureRecognizer.Direction.right:
+            sRight  = true
+            sLeft = false
+            print("right swipe")
+            self.count = 0
+            self.moveRight()
+        case UISwipeGestureRecognizer.Direction.left:
+            sLeft = true
+            sRight = false
+            print("left swipe")
+            self.count = 1
+            self.moveUp()
+        default:
+            break
+        }
     }
+    
+    
+    
+    
+    
     
 }
 
